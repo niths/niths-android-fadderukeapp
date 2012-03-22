7e37d7a1c7a67edf95abfc3a4872a8d37012d24f
whirlwin <whirlwin90@gmail.com>
2012-03-22 10:28:33 +0100

    Added some files.

--- assets/www/javascripts/index.js
+++ assets/www/javascripts/index.js
@@ -1,7 +1,18 @@
-google.load('gdata', '2.x');
 
 $(document).ready(function() {
-  $('#login').click(function() {
+  $('form').submit(function() {
+    $.ajax({
+      url: "http://10.0.2.2:55404/niths/google/login",
+      type: "post",
+      data: $('form').serialize(),
+      success: function(status, data) {
+        alert("SUCCESS: " + status + data);
+      },
+      error: function(xhr, status) {
+        alert(JSON.stringify(xhr) + status);
+      }
+    });
     
+    return false;
   });
 });

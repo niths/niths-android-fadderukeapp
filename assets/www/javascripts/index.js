15e17b6e15f0766417fa3f00a01d96edf5c14ab2
whirlwin <whirlwin90@gmail.com>
2012-03-24 22:09:55 +0100

    Added login check.

--- assets/www/javascripts/index.js
+++ assets/www/javascripts/index.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var callbackURL      = "http://niths.no/callback";
-  var stateURLFragment = "state=/profile"; 
+  var stateURLFragment = "state=/profile";
+  var isNITHMail       = false;
 
   $('#login').click(function() {
     signIn(); 
   });
 
   function signIn() {
-    configure(); 
+    configureLocationChanged(); 
     window.plugins.childBrowser.showWebPage(
       "https://accounts.google.com/o/oauth2/auth"
       + "?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email"
@@ -18,22 +19,39 @@ $(document).ready(function() {
       { showLocationBar: false});
   };
 
-  function configure() {
+  function configureLocationChanged() {
     window.plugins.childBrowser.onLocationChange = function(url) {
+      alert(url);
       var receiveTokenURL = new RegExp("^" + callbackURL + '#' +
         stateURLFragment + "&access_token=..*$");
 
+      if (url == "https://www.google.com/a/nith.no/acs") {
+        isNITHMail = true;
+      }
+
       // Triggered if the app is denied access
-      if (url == callbackURL + "#error=access_denied" + stateURLFragment) {
-        $('#error').html("<strong>Could not approve app</strong>");
+      else if (url == callbackURL + "#error=access_denied" + stateURLFragment) {
+        displayError("Kunne ikke få tilgang");
         window.plugins.childBrowser.close();
 
       // Triggered if a token is received
       } else if (receiveTokenURL.test(url)) {
         window.plugins.childBrowser.close();
-        window.location = "main-menu.html"
+
+        if (isNITHMail) {
+          window.plugins.childBrowser.close();
+          window.location = "main-menu.html";
+        } else {
+          window.plugins.childBrowser.showWebPage(
+            "https://accounts.google.com/Logout?continue=http://google.com");
+          // window.plugins.childBrowser.close();
+          displayError("Vennligst logg inn med en NITH e-postadresse");
+        }
       }
-      // alert(url);
     };
+  };
+
+  function displayError(error) {
+    $('#error').html("<strong>" + error + "</strong>");
   };
 });

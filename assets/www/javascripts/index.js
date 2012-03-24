37af8f83d6512d8651ca17d9249bcaf65cfcf675
whirlwin <whirlwin90@gmail.com>
2012-03-24 20:12:47 +0100

    Made logging in work.

--- assets/www/javascripts/index.js
+++ assets/www/javascripts/index.js
@@ -1,23 +1,40 @@
 $(document).ready(function() {
+  var callbackURL        = "http://niths.no/callback";
+  var stateURLFragment = "state=/profile"; 
+
   $('#login').click(function() {
-    signIn();
+    signIn(); 
   });
 
   function signIn() {
-    configure();
+    configure(); 
     window.plugins.childBrowser.showWebPage(
       "https://accounts.google.com/o/oauth2/auth"
       + "?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email"
-      + "&state=%2Fprofile"
-      + "&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fniths%2Fgoogle%2Fresult"
+      + '&' + stateURLFragment
+      + "&redirect_uri=" + encodeURIComponent(callbackURL)
       + "&response_type=token"
-      + "&client_id=1064171706637-ep2hk9dqivvp87gh18jn7c0hm9bd1icn.apps.googleusercontent.com",
+      + "&client_id=1064171706637-f9efklqg3tbrmu7fctvk8khvc0dqmh5i.apps.googleusercontent.com",
       { showLocationBar: false});
   };
 
   function configure() {
     window.plugins.childBrowser.onLocationChange = function(url) {
-      alert(url);
+      var receiveTokenURL = new RegExp("^" + callbackURL + '#' +
+    		  stateURLFragment + "&access_token=..*$");
+      console.log(url);
+
+      // Triggered if the app is denied access
+      if (url == callbackURL + "#error=access_denied" + stateURLFragment) {
+        $('#error').html("<strong>Could not approve app</strong>");
+        window.plugins.childBrowser.close();
+
+      // Triggered if a token is received
+      } else if (receiveTokenURL.test(url)) {
+        window.plugins.childBrowser.close();
+        
+      }
+      // alert(url);
     };
   };
 });

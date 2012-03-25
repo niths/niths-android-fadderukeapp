14e156b04f2d23baab9bafddc6403714c39f2ed2
whirlwin <whirlwin90@gmail.com>
2012-03-25 15:42:11 +0200

    Fixed logout bug.

--- assets/www/javascripts/index.js
+++ assets/www/javascripts/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  var callbackURL      = "http://niths.no/callback";
-  var stateURLFragment = "state=/profile";
+  var callbackURL      = 'http://niths.no/callback';
+  var stateURLFragment = 'state=/profile';
   var isNITHMail       = false;
 
   $('#login').click(function() {
@@ -10,28 +10,28 @@ $(document).ready(function() {
   function signIn() {
     configureLocationChanged(); 
     window.plugins.childBrowser.showWebPage(
-      "https://accounts.google.com/o/oauth2/auth"
-      + "?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email"
+      'https://accounts.google.com/o/oauth2/auth'
+      + '?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email'
       + '&' + stateURLFragment
-      + "&redirect_uri=" + encodeURIComponent(callbackURL)
-      + "&response_type=token"
-      + "&client_id=1064171706637-f9efklqg3tbrmu7fctvk8khvc0dqmh5i.apps.googleusercontent.com",
+      + '&redirect_uri=' + encodeURIComponent(callbackURL)
+      + '&response_type=token'
+      + '&client_id=1064171706637-f9efklqg3tbrmu7fctvk8khvc0dqmh5i.apps.googleusercontent.com',
       { showLocationBar: false});
   };
 
   function configureLocationChanged() {
     window.plugins.childBrowser.onLocationChange = function(url) {
       alert(url);
-      var receiveTokenURL = new RegExp("^" + callbackURL + '#' +
-        stateURLFragment + "&access_token=..*$");
+      var receiveTokenURL = new RegExp('^' + callbackURL + '#' +
+        stateURLFragment + '&access_token=..*$');
 
-      if (url == "https://www.google.com/a/nith.no/acs") {
+      if (url == 'https://www.google.com/a/nith.no/acs') {
         isNITHMail = true;
       }
 
       // Triggered if the app is denied access
-      else if (url == callbackURL + "#error=access_denied" + stateURLFragment) {
-        displayError("Kunne ikke få tilgang");
+      else if (url == callbackURL + '#error=access_denied' + stateURLFragment) {
+        displayError('Kunne ikke få tilgang');
         window.plugins.childBrowser.close();
 
       // Triggered if a token is received
@@ -39,19 +39,29 @@ $(document).ready(function() {
         window.plugins.childBrowser.close();
 
         if (isNITHMail) {
+          $('#login').css('visibility', 'hidden');
           window.plugins.childBrowser.close();
-          window.location = "main-menu.html";
+          window.location = 'main-menu.html';
         } else {
-          window.plugins.childBrowser.showWebPage(
-            "https://accounts.google.com/Logout?continue=http://google.com");
-          // window.plugins.childBrowser.close();
-          displayError("Vennligst logg inn med en NITH e-postadresse");
+          $('#logout').css('visibility', 'visible');
+          displayError('Vennligst logg inn med en NITH e-postadresse');
         }
+
+      // Triggered when a user logs out
+      } else if (url == 'https://accounts.google.com/Login') {
+        window.plugins.childBrowser.close();
+        $('#error').empty();
+        $('#logout').css('visibility', 'hidden');
       }
     };
   };
 
   function displayError(error) {
-    $('#error').html("<strong>" + error + "</strong>");
+    $('#error').html('<strong>' + error + '</strong>');
   };
+
+  $('#logout').click(function(event) {
+      window.plugins.childBrowser.showWebPage(
+      'https://accounts.google.com/Logout');
+  });
 });

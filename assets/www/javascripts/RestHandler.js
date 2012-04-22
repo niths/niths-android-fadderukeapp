8deb8a0b1f3dc8766c3f1359f2528aec2305e31e
Bendik Rostad <bendikr@gmail.com>
2012-04-22 13:20:17 +0200

    Changed urls to ec2

--- assets/www/javascripts/RestHandler.js
+++ assets/www/javascripts/RestHandler.js
@@ -15,10 +15,10 @@
  * });
  */
 function RestHandler(){
-	this.baseUrl = 'http://192.168.0.105:8080/niths/'; // Ben
+//	this.baseUrl = 'http://192.168.0.105:8080/niths/'; // Ben
 	
 //	this.baseUrl = 'http://10.21.24.105:8080/niths/';
-//	this.baseUrl = 'http://ec2-46-137-44-111.eu-west-1.compute.amazonaws.com:8080/niths/';
+	this.baseUrl = 'http://ec2-46-137-44-111.eu-west-1.compute.amazonaws.com:8080/niths/';
 	
 	this.find = function(modelUrl, callbackSuccess, callbackError) {
 	    $.ajax({
--- assets/www/javascripts/common/application.js
+++ assets/www/javascripts/common/application.js
@@ -1,7 +1,7 @@
 
 //address = 'http://192.168.1.157:8080/niths/';
-address = 'http://192.168.0.105:8080/niths/'; //Ben
-//address = 'http://ec2-46-137-44-111.eu-west-1.compute.amazonaws.com:8080/niths/';
+//address = 'http://192.168.0.105:8080/niths/'; //Ben
+address = 'http://ec2-46-137-44-111.eu-west-1.compute.amazonaws.com:8080/niths/';
 student = {};
 sessionToken = '';
 

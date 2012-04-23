2ed64aff771a8994b4d042361a4f23a364f9e9ef
Oyvind Odegard <whirlwin90@gmail.com>
2012-04-23 11:41:27 +0200

    Added cors.

--- assets/www/javascripts/index/index.js
+++ assets/www/javascripts/index/index.js
@@ -2,6 +2,7 @@ $("#dashboard-page").live('pageinit', function() {
 	//TODO: REMOVE ON iOS AND WP7 --> Android doesnt handle transitions well
 	$.mobile.defaultPageTransition = 'none';
 	$.mobile.defaultDialogTransition = 'none';
+	$.support.cors = true;
 	/////////////////////////////////
 	
 	var restClient = new RestHandler(); //REST CLIENT

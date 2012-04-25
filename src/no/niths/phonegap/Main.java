7391a64b21dbf376ff48a38232c79d39efcbf0a0
Bendik Rostad <bendikr@gmail.com>
2012-04-25 11:10:41 +0200

    Added splashscreen for android

--- src/no/niths/phonegap/Main.java
+++ src/no/niths/phonegap/Main.java
@@ -9,6 +9,8 @@ public class Main extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl("file:///android_asset/www/index.html");
+        
     }
 }

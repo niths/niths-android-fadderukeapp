2843089a9e6544b495fc7954260d42fb5aed0ac6
Bendik Rostad <bendikr@gmail.com>
2012-04-26 16:53:00 +0200

    Re-added Flurry (it got lost somehow)

--- src/no/niths/phonegap/Main.java
+++ src/no/niths/phonegap/Main.java
@@ -2,6 +2,7 @@ package no.niths.phonegap;
 
 import android.os.Bundle;
 
+import com.flurry.android.FlurryAgent;
 import com.phonegap.DroidGap;
 
 public class Main extends DroidGap {
@@ -12,5 +13,18 @@ public class Main extends DroidGap {
         super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl("file:///android_asset/www/index.html");
         
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FlurryAgent.onStartSession(this, "3C8DZ6EVBHJ3IR1T7X1S");
+    }
+
+    @Override
+    public void onStop()
+    {
+       super.onStop();
+       FlurryAgent.onEndSession(this);
     }
 }

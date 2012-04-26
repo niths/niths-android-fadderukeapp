334a45113238b5b4d7ae81bfe07df74eb6219846
Bendik Rostad <bendikr@gmail.com>
2012-04-26 10:29:24 +0200

    force pushed application.js

--- assets/www/javascripts/common/application.js
+++ assets/www/javascripts/common/application.js
@@ -1,12 +1,22 @@
 //address = 'http://192.168.0.105:8080/niths/'; //Ben
-//address = 'http://10.110.73.58:8080/niths/';
+
 address = 'http://ec2-46-137-44-111.eu-west-1.compute.amazonaws.com:8080/niths/';
 
 student = {};
 sessionToken = '';
 
-applicationKey = 'NH3E5WANTJ';
-applicationToken = 'JJbOUVRgFzOvGGUicKnuhB+fXeyvEnJYpl514ar3nU91brUX1EgisRWwEdj1Byw3CeKjPSnqSSO70xwxmupwjw==';
-developerToken = 'Q9WduujDMII1vTqpnoefwSx7HWkuqOmtdure1g8BCWA1EGN6nPCNQj4KjtuYwIA0j1CnoC5OQE5UvAfq2tFylw==';
-developerKey = 'sob8gvpyk2';
+applicationKey = '';
+applicationToken ='';
+developerToken ='';
+developerKey ='';
 
+
+htmlEncode = function(s) {
+	 encodedHtml = escape(s);
+	 encodedHtml = encodedHtml.replace(/\//g,"%2F");
+	 encodedHtml = encodedHtml.replace(/\?/g,"%3F");
+	 encodedHtml = encodedHtml.replace(/=/g,"%3D");
+	 encodedHtml = encodedHtml.replace(/&/g,"%26");
+	 encodedHtml = encodedHtml.replace(/@/g,"%40");
+	 return encodedHtml;
+}

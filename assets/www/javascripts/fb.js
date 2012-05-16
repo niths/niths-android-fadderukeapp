993c932081ca2eedcc8589b439c9b5ddeef96f67
Bendik Rostad <bendikr@gmail.com>
2012-05-16 16:51:24 +0200

    Small fixes to gallery

--- assets/www/javascripts/fb.js
+++ assets/www/javascripts/fb.js
@@ -1,61 +1,77 @@
-$('#Gallery1').live('pageshow', function() {
-  getLatestImages();
+$('#Gallery1')
+		.live(
+				'pageshow',
+				function(e) {
+					refresh();
 
-  function showImgs(){
-    $('#galleryloader').css('display', 'none');	
-    $('#gallerycontent').css('visibility', 'visible');
-  }
+					function showImgs() {
+						$('#galleryloader').css('display', 'none');
+						$('#gallerycontent').css('visibility', 'visible');
+					}
 
-  $('#refreshGalleryBtn').click(function() {
-    refresh();
-  });
+					$('#refreshGalleryBtn').click(function() {
+						refresh();
+					});
 
-  function refresh() {
-    $('#galleryloader').css('display', 'block');
-    $('#gallerycontent').css('visibility', 'hidden');
+					function refresh() {
+						$('#galleryloader').css('display', 'block');
+						$('#gallerycontent').css('visibility', 'hidden');
 
-    getLatestImages();
-  }
+						getLatestImages();
+					}
 
-  /**
-   * Selects an album from facebook
-   * Replace the id with the album id you want to display
-   * the lastest 25. 
-   * 
-   * Open browser, go to facebook, select album, copy paste id from url
-   */
-  function getLatestImages(){
-    $.ajax({
-      url:      'https://graph.facebook.com/10150304760787369/photos',
-      dataType: 'json',
-      type:     'GET',
-      timeout:  5000,
-      success:  function(album){
-        handleImgsData(album.data);
-      },
-      error:    function(){
-        showErr('Ikke kontakt med facebook', null);
-      }
-    });
-  }
+					/**
+					 * Selects an album from facebook Replace the id with the
+					 * album id you want to display the lastest 25.
+					 * 
+					 * Open browser, go to facebook, select album, copy paste id
+					 * from url
+					 */
+					function getLatestImages() {
+						$
+								.ajax({
+									url : 'https://graph.facebook.com/10150304760787369/photos',
+									dataType : 'json',
+									type : 'GET',
+									timeout : 5000,
+									success : function(album) {
+										handleImgsData(album.data);
+									},
+									error : function() {
+										showErr('Ikke kontakt med facebook',
+												null);
+									}
+								});
+					}
 
-  /**
-   * Selects two images, one low res thumb, on normal
-   * from facebook json response
-   */
-  function handleImgsData(images){
-    var html = '';
-    for (var i = 0; i < images.length; i++) {
-      var imgT = images[i].images[6]['source'];
-      var imgN = images[i].images[4]['source'];
-      $('#lastPics').append(
-          '<li><a href="' + imgN + '" rel="external">' +
-          '<img src="'+imgT+'" alt="NITHs" /></a></li>');
-    }
-    if(Code.PhotoSwipe.getInstance('Gallery1') == null){
-      $("ul.gallery a").photoSwipe(null,  'Gallery1');      
-    }
+					/**
+					 * Selects two images, one low res thumb, on normal from
+					 * facebook json response
+					 */
+					function handleImgsData(images) {
+						var html = '';
+						for ( var i = 0; i < images.length; i++) {
+							var imgT = images[i].images[6]['source'];
+							var imgN = images[i].images[4]['source'];
+							$('#lastPics').append(
+									'<li><a href="' + imgN
+											+ '" rel="external">'
+											+ '<img src="' + imgT
+											+ '" alt="NITHs" /></a></li>');
+						}
 
-    showImgs();
-  }
-});
+						var currentPage = $(e.target);
+						var p = Code.PhotoSwipe.getInstance(currentPage
+								.attr('id'));
+
+						if (p != null) {
+							Code.PhotoSwipe.detatch(p);
+						}
+
+						var currentPage = $(e.target), options = {};
+						$("ul.gallery a", e.target).photoSwipe(options,
+								currentPage.attr('id'));
+
+						showImgs();
+					}
+				});
